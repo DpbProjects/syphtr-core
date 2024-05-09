8ac614599ce98351795e39c3775f4444eaeb9c2f
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-05-09 18:04:36 +0100

    feat: install shadcn ui + add sidenav component

--- src/components/layout/sidenav.tsx
+++ src/components/layout/sidenav.tsx
@@ -1,31 +1,132 @@
+"use client";
+
+import { useState, type ReactNode } from "react";
+
+import Image from "next/image";
 import Link from "next/link";
 
-import NavLinks from "@/components/layout/nav-links";
+import { RocketIcon, User, Cable, Book, Users } from "lucide-react";
 
-import { PowerIcon } from "@heroicons/react/24/outline";
+type SideNavProps = {
+  children: ReactNode;
+};
 
-export default function SideNav() {
+import { Sidebar, useSidebar, Overlay, Button } from "@rewind-ui/core";
+
+export default function SideNav({ children }: SideNavProps) {
+  const [expanded, setExpanded] = useState(true);
+  const [mobile, setMobile] = useState(false);
+  const sidebar = useSidebar();
+
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-green-600 p-4 md:h-40"
-        href="/"
+    <div className="relative flex min-h-full w-full flex-row">
+      <Sidebar
+        onToggle={(state) => {
+          setExpanded(state.expanded);
+          setMobile(state.mobile);
+        }}
+        className="absolute"
+        color="zinc"
       >
-        <div className="w-32 text-3xl text-white md:w-40">
-          {/* <AcmeLogo /> */}
-          Syphtr
-        </div>
-      </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-green-100 hover:text-green-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
-      </div>
+        <Sidebar.Head>
+          <Sidebar.Head.Logo>
+            <Image src="/favicon.ico" width={48} height={48} alt="Rewind-UI" />
+          </Sidebar.Head.Logo>
+          <Sidebar.Head.Title>Syphtr</Sidebar.Head.Title>
+          {/* <Sidebar.Head.Toggle /> */}
+        </Sidebar.Head>
+
+        <Sidebar.Nav>
+          <Sidebar.Nav.Section>
+            <Sidebar.Nav.Section.Item
+              icon={<RocketIcon />}
+              label="Dashboard"
+              href="#"
+              active
+            />
+          </Sidebar.Nav.Section>
+
+          <Sidebar.Nav.Section>
+            <Sidebar.Nav.Section.Title>Search</Sidebar.Nav.Section.Title>
+            <Link href="/dashboard/search">
+              <Sidebar.Nav.Section.Item
+                icon={<User />}
+                label="Find profiles"
+                as={"div"}
+              />
+            </Link>
+
+            <Sidebar.Nav.Section.Item
+              icon={<Cable />}
+              label="Pipelines"
+              href="#"
+            />
+          </Sidebar.Nav.Section>
+          <Sidebar.Nav.Section>
+            <Sidebar.Nav.Section.Title>Management</Sidebar.Nav.Section.Title>
+            <Sidebar.Nav.Section.Item
+              icon={<Book />}
+              label="Jobs"
+              href="jobs"
+            />
+            <Sidebar.Nav.Section.Item icon={<Users />} label="Team" href="#" />
+          </Sidebar.Nav.Section>
+        </Sidebar.Nav>
+
+        <Sidebar.Footer>
+          <div className="flex flex-col items-center justify-center text-sm">
+            <span className="font-semibold">Rewind-UI</span>
+            <span>version x.y.z</span>
+          </div>
+        </Sidebar.Footer>
+      </Sidebar>
+
+      <main
+        className={`flex w-full transform flex-col items-center text-slate-700 transition-all duration-100 ${
+          expanded ? "md:ml-64" : "md:ml-20"
+        }`}
+      >
+        {mobile && (
+          <Overlay
+            blur="none"
+            onClick={() => {
+              sidebar.toggleMobile();
+            }}
+            className="z-40 md:hidden"
+          />
+        )}
+        <header className="sticky top-0 flex min-h-[4rem] w-full flex-row items-center border-b border-b-gray-100 bg-white px-8 shadow-sm">
+          <span>Navbar</span>
+
+          <Button
+            onClick={() => {
+              sidebar.toggleMobile();
+            }}
+            size="sm"
+            color="white"
+            icon
+            className="ml-auto flex md:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M448 96c0-17.7-14.3-32-32-32H32C14.3 64 0 78.3 0 96s14.3 32 32 32H416c17.7 0 32-14.3 32-32zm0 320c0-17.7-14.3-32-32-32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32z" />
+              <path
+                className="opacity-50"
+                d="M0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32z"
+              />
+            </svg>
+          </Button>
+        </header>
+
+        <div className="h-full w-full p-8">{children}</div>
+
+        {/* <div className="flex sticky bottom-0 items-center bg-white w-full min-h-[4rem] px-8">
+          <span>Footer</span>
+        </div> */}
+      </main>
     </div>
   );
 }
