7690eaacdc646cc3c3807fbde7b813be71618b48
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-05-28 21:13:28 +0100

    refactor: profile form to use zod validation

--- src/components/search/profile-form.tsx
+++ src/components/search/profile-form.tsx
@@ -1,64 +1,59 @@
 "use client";
 
-import { useRef, type FormEvent, type MouseEvent } from "react";
-
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
-import type { FormValues } from "@/utils/types";
+import type { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { formSchema } from "@/utils/schema";
 
 export default function ProfileForm() {
-  const formRef = useRef<HTMLFormElement>(null);
   const searchParams = useSearchParams();
   const pathname = usePathname();
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { replace } = useRouter();
 
-  const buildParameters = (formValues: FormValues | null) => {
+  const { register, handleSubmit, reset } = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      city: "",
+      state: "",
+      country: "",
+      pastJobTitle: "",
+      pastJobCompanyName: "",
+    },
+  });
+
+  const buildParameters = (formValues: Record<string, string | null>) => {
     const params = new URLSearchParams(searchParams);
 
-    for (const value in formValues) {
-      if (formValues[value as keyof FormValues] !== "") {
-        params.set(value, formValues[value as keyof FormValues]);
+    for (const [key, value] of Object.entries(formValues)) {
+      if (value) {
+        params.set(key, value);
       } else {
-        params.delete(value);
+        params.delete(key);
       }
     }
-
-    if (formValues === null) {
-      replace(pathname);
-    }
-
-    // params.set("query", formValues["firstName"]);
 
     replace(`${pathname}?${params.toString()}`);
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-
-    const formValues = {
-      fullName: formData.get("fullName") as string,
-      city: formData.get("city") as string,
-      state: formData.get("state") as string,
-      country: formData.get("country") as string,
-      pastJobTitle: formData.get("pastJobTitle") as string,
-      pastJobCompanyName: formData.get("pastJobCompanyName") as string,
-    };
-
-    buildParameters(formValues);
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    buildParameters(data);
   };
 
-  const handleReset = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleReset = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    formRef?.current?.reset();
-    replace(pathname);
+    reset(); // Reset form using react-hook-form's reset method
+    replace(pathname); // Reset URL parameters
   };
 
   return (
     <div className="h-full w-full max-w-md overflow-x-hidden overflow-y-scroll rounded-lg bg-gray-100 p-8 shadow-lg">
       <h2 className="mb-6 text-left text-xl font-bold">Personal details</h2>
-      <form ref={formRef} className="space-y-4 " onSubmit={handleSubmit}>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label
             htmlFor="fullName"
@@ -68,8 +63,8 @@ export default function ProfileForm() {
           </label>
           <input
             type="text"
-            name="fullName"
             id="fullName"
+            {...register("fullName")}
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           />
         </div>
@@ -78,12 +73,12 @@ export default function ProfileForm() {
             htmlFor="city"
             className="block text-sm font-medium text-gray-700"
           >
-            Country
+            City
           </label>
           <input
             type="text"
-            name="city"
             id="city"
+            {...register("city")}
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           />
         </div>
@@ -92,12 +87,12 @@ export default function ProfileForm() {
             htmlFor="state"
             className="block text-sm font-medium text-gray-700"
           >
-            City
+            State
           </label>
           <input
             type="text"
-            name="state"
             id="state"
+            {...register("state")}
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           />
         </div>
@@ -110,8 +105,8 @@ export default function ProfileForm() {
           </label>
           <input
             type="text"
-            name="country"
             id="country"
+            {...register("country")}
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           />
         </div>
@@ -127,8 +122,8 @@ export default function ProfileForm() {
           </label>
           <input
             type="text"
-            name="pastJobTitle"
             id="pastJobTitle"
+            {...register("pastJobTitle")}
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           />
         </div>
@@ -141,8 +136,8 @@ export default function ProfileForm() {
           </label>
           <input
             type="text"
-            name="pastJobCompanyName"
             id="pastJobCompanyName"
+            {...register("pastJobCompanyName")}
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           />
         </div>
