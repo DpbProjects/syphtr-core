55e9356cd383cb3b95eeef0898b76ee6220c3465
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-05-28 13:16:53 +0100

    refactor: remove unused data fetchers

--- src/app/dashboard/search/page.tsx
+++ src/app/dashboard/search/page.tsx
@@ -1,9 +1,5 @@
 import { Suspense } from "react";
-import {
-  fetchSharedProfiles,
-  fetchSharedProfilesPages,
-  fetchSharedProfileCount,
-} from "@/utils/data";
+import { fetchSharedProfiles } from "@/utils/data";
 
 import ProfileCard from "@/components/search/profile-card";
 import ProfileForm from "@/components/search/profile-form";
@@ -15,16 +11,11 @@ export default async function Page({
   searchParams: FormValues;
 }) {
   const query = searchParams ?? {};
-  // const currentPage = Number(searchParams?.page) || 1;
 
   const sharedProfiles = await fetchSharedProfiles(query);
 
-  const sharedProfileCount = await fetchSharedProfileCount();
-
   const hasParams = Object.keys(query).length !== 0;
   const hasProfiles = sharedProfiles.length !== 0;
-
-  console.log(sharedProfiles);
 
   const Loading = () => <div>Loading......</div>;
 
@@ -35,15 +26,11 @@ export default async function Page({
           <div className=" min-w-[712px]">
             {hasParams && hasProfiles ? (
               sharedProfiles?.map((profile) => {
-                const { SharedRawProfile, Education, Experience } = profile;
+                // const { SharedRawProfile, Education, Experience } = profile;
                 return (
                   <ProfileCard
-                    key={profile.SharedRawProfile.publicIdentifier}
-                    profileData={{
-                      SharedRawProfile,
-                      Education,
-                      Experience,
-                    }}
+                    key={profile.publicIdentifier}
+                    profileData={profile}
                   />
                 );
               })
@@ -51,7 +38,7 @@ export default async function Page({
               <div>No results</div>
             )}
           </div>
-          <div className="sticky top-16 flex h-screen min-h-screen w-full overflow-y-auto overflow-x-hidden rounded-lg bg-gray-100">
+          <div className="sticky top-16 flex h-screen min-h-screen w-full  rounded-lg bg-gray-100">
             <ProfileForm />
           </div>
         </div>
